550594d5d5b7bfee0a380f34eb009b6ca6a2669a
Uriel <uriel.avila95@gmail.com>
2018-12-05 10:32:10 -0600

    Refactor: Separación de funcionalidad en 2 funciones
    Se separa la funcionalidad de validate en 2 funciones. La getIds obtiene los ID's, y la validateIds los valida.

--- java/mtax/MTax.java
+++ java/mtax/MTax.java
@@ -7,49 +7,57 @@ public class MTax implements Constant {
     public MTax() {
     }
 
-    public static List<String> validate(List<XTax> xTaxList) {
+    private static void getIds(List<XTax> xTaxList, List<String> errorList, List<String> validIds) {
 
-        List<String> errorList = new ArrayList<>();
-
-        if (xTaxList != null && xTaxList.size() > 0) {
-            List<String> validIds = new ArrayList<>();
-            int cont = 0;
-            for (XTax tax : xTaxList) {
-                if (tax.getId() != null) {
-                    validIds.add(tax.getId().toString());
-                }
-
-                if (tax.getTax() == null) {
-                    errorList.add("El impuesto es obligatorio");
-                }
-
-                if (!tax.isLocal()) {
-                    cont++;
-                }
+        int cont = 0;
+        for (XTax tax : xTaxList) {
+            if (tax.getId() != null) {
+                validIds.add(tax.getId().toString());
             }
-            if (cont <= 0) {
-                errorList.add("Debe de incluir al menos una tasa no local");
-            }
-            if (validIds.size() > 0) {
 
-                List<XTax> xt = TaxsByListId(validIds, false);
-                if (xt.size() != validIds.size()) {
-                    errorList.add("Existen datos no guardados previamente");
-                } else {
-                    HashMap<String, XTax> map_taxs = new HashMap<String, XTax>();
-                    for (XTax tax : xt) {
-                        map_taxs.put(tax.getId().toString(), tax);
-                    }
-                    for (int i = 0; i < xTaxList.size(); i++) {
-                        if (xTaxList.get(i).getId() != null) {
-                            xTaxList.get(i).setCreated(map_taxs.get(xTaxList.get(i).getId().toString()).getCreated());
-                        }
+            if (tax.getTax() == null) {
+                errorList.add("El impuesto es obligatorio");
+            }
+
+            if (!tax.isLocal()) {
+                cont++;
+            }
+        }
+        if (cont <= 0) {
+            errorList.add("Debe de incluir al menos una tasa no local");
+        }
+    }
+
+    private static void validateIds(List<XTax> xTaxList, List<String> errorList, List<String> validIds) {
+        if (validIds.size() > 0) {
+
+            List<XTax> xt = TaxsByListId(validIds, false);
+            if (xt.size() != validIds.size()) {
+                errorList.add("Existen datos no guardados previamente");
+            } else {
+                HashMap<String, XTax> map_taxs = new HashMap<String, XTax>();
+                for (XTax tax : xt) {
+                    map_taxs.put(tax.getId().toString(), tax);
+                }
+                for (int i = 0; i < xTaxList.size(); i++) {
+                    if (xTaxList.get(i).getId() != null) {
+                        xTaxList.get(i).setCreated(map_taxs.get(xTaxList.get(i).getId().toString()).getCreated());
                     }
                 }
             }
         }
+    }
+
+    public static List<String> validate(List<XTax> xTaxList) {
+
+        List<String> errorList = new ArrayList<>();
+        List<String> validIds = new ArrayList<>();
+
+        if (xTaxList != null && xTaxList.size() > 0) {
+            getIds(xTaxList, errorList, validIds);
+            validateIds(xTaxList, errorList, validIds);
+        }
 
         return errorList;
     }
-
 }
